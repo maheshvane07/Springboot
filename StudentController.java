5436ee693ae79d789f86984ea2586e746fbc391b
maheshvane07 <131536495+maheshvane07@users.noreply.github.com>
2023-04-25 20:53:04 +0530

    Add files via upload

---
+++ StudentController.java
@@ -0,0 +1,49 @@
+package com.qsp.studentrestapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qsp.stdentrestapi.dto.Student;
+import com.qsp.studentrestapi.repositary.StudentRepositary;
+
+@RestController
+public class StudentController {
+	@Autowired
+	StudentRepositary sr;
+	
+	@PostMapping("/savestudent")
+	public String saveStudent(@RequestBody Student s)
+	{
+		sr.save(s);
+		return "Student saved";
+	}
+	
+	@GetMapping("/getstudent")
+	public Student getStudentById(@RequestParam int id)
+	{
+		return sr.findById(id).get();
+	}
+	
+	@PutMapping("/updatestudent")
+	public String update(@RequestBody Student s)
+	{
+		sr.save(s);
+		return "student updated";
+	}
+	
+	
+	@DeleteMapping("/deletedata")
+	public String deletestudentById(@RequestParam int id)
+	{
+		sr.deleteById(id);
+		return "student deleted";
+	}
+	
+
+}
